e86c06f30d955a42287a46602923e52a3d96d5fc
Rodion <radov.2011@gmail.com>
2021-10-13 15:01:27 +0300

    TPM attempt to add new env context

--- src/main/java/core/builder/ContextBuilder.java
+++ src/main/java/core/builder/ContextBuilder.java
@@ -1,13 +1,16 @@
 package core.builder;
 
 
+import core.context.EnvironmentContext;
+import org.aeonbits.owner.ConfigFactory;
 
 public class ContextBuilder {
 
+    private static EnvironmentContext environmentContext;
 
-    /*public static EnvironmentContext getEnvironmentContext() {
+    public static EnvironmentContext getEnvironmentContext() {
         return environmentContext != null ? environmentContext :
                 (environmentContext = ConfigFactory.create(EnvironmentContext.class, System.getProperties()));
-    }*/
+    }
 
 }
