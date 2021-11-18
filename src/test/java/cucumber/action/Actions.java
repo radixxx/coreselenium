c8a396fcaa43c7109199cd2117ce0389c08e1f41
radixxx <radov.2011@gmail.com>
2021-11-18 14:13:24 +0200

    TMP

---
+++ src/test/java/cucumber/action/Actions.java
@@ -0,0 +1,24 @@
+package cucumber.action;
+
+import org.openqa.selenium.WebDriver;
+import site.ui.actions.base.BaseActions;
+import site.ui.pom.google.GooglePage;
+
+
+public class Actions extends BaseActions{
+
+    private final GooglePage googlePage;
+
+    public Actions(WebDriver driver) {
+        super(driver);
+        googlePage = new GooglePage(driver);
+    }
+
+    public void changeLanguagePage_RU(){
+        googlePage.clickOnRussionLanguageButton();
+    }
+
+    public void changeLanguagePage_RO(){
+        googlePage.clickOnRomanianLanguageButton();
+    }
+}
--- src/test/java/cucumber/step/GeneralStepDefinitions.java
+++ src/test/java/cucumber/step/GeneralStepDefinitions.java
@@ -1,24 +1,22 @@
 package cucumber.step;
 
+import cucumber.action.Actions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import site.ui.actions.base.BaseActions;
 import site.ui.pom.google.GooglePage;
+import utils.Config;
 
 public class GeneralStepDefinitions {
-
-    private GooglePage googlePage;
-    private BaseActions baseActions;
-
-    public GeneralStepDefinitions(WebDriver driver) {
-        googlePage = new GooglePage(driver);
-    }
+    private WebDriver driver;
+    protected Actions actions;
+    protected GooglePage googlePage;
 
     @Given("navigate to page")
     public void navigateTo() {
-         this.baseActions.navigate();
+        actions.navigate("https://www.google.com/");
     }
 
     @When("change page lang to RO")
